6a809e912618de1d108035819609f9dab9f7d8c2
Jared Tobin <jared@jtobin.io>
2026-04-18 13:46:46 +0800

    flake: fix missing llvm derivations

--- flake.nix
+++ flake.nix
@@ -11,16 +11,20 @@
       type = "git";
       url  = "git://git.ppad.tech/sha256.git";
       ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
     };
     ppad-secp256k1 = {
       type = "git";
       url  = "git://git.ppad.tech/secp256k1.git";
       ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
+      inputs.ppad-sha256.follows = "ppad-sha256";
     };
     ppad-ripemd160 = {
       type = "git";
       url  = "git://git.ppad.tech/ripemd160.git";
       ref  = "master";
+      inputs.ppad-nixpkgs.follows = "ppad-nixpkgs";
     };
     flake-utils.follows = "ppad-nixpkgs/flake-utils";
     nixpkgs.follows = "ppad-nixpkgs/nixpkgs";
@@ -38,13 +42,27 @@
         clang = pkgs.llvmPackages_19.clang;
 
         sha256 = ppad-sha256.packages.${system}.default;
+        sha256-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag sha256 "llvm")
+            [ llvm clang ];
+
         secp256k1 = ppad-secp256k1.packages.${system}.default;
+        secp256k1-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag secp256k1 "llvm")
+            [ llvm clang ];
+
         ripemd160 = ppad-ripemd160.packages.${system}.default;
+        ripemd160-llvm =
+          hlib.addBuildTools
+            (hlib.enableCabalFlag ripemd160 "llvm")
+            [ llvm clang ];
 
         hpkgs = pkgs.haskell.packages.ghc910.extend (new: old: {
-          ppad-sha256 = sha256;
-          ppad-secp256k1 = secp256k1;
-          ppad-ripemd160 = ripemd160;
+          ppad-sha256 = sha256-llvm;
+          ppad-secp256k1 = secp256k1-llvm;
+          ppad-ripemd160 = ripemd160-llvm;
           ${lib} = new.callCabal2nix lib ./. { };
         });
 
